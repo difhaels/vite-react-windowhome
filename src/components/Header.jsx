8ad2812b67ef6a0203459537da663e17fe6ad9e0
Agung Saputra <agungsaputradifh@gmail.com>
2024-07-31 07:19:58 +0700

    test bootstrap

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">LandingPage</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
