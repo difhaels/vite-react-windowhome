532b2311751be35531e72da40199122ef4ffac6b
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-02 21:38:31 +0700

    edit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,10 @@ function Header() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#about">Tentang Kami</Nav.Link>
+            <Nav.Link href="#layanan">Layanan</Nav.Link>
+            <Nav.Link href="#produk">Produk</Nav.Link>
+            <Nav.Link href="#kontak">Kontak</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
