67839755da5144315a43d413a1a465e3740b23cb
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-02 21:27:35 +0700

    features v2

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,41 +1,42 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import { FaUsers, FaDollarSign, FaRocket, FaShieldAlt } from 'react-icons/fa';
+import '../assets/css/features.css';
 
 function Features() {
   return (
     <Container className="my-5">
-      <h2 className="text-center">Features</h2>
-      <Row>
-        <Col lg={3} md={6} sm={6} xs={12} className="mb-4">
-          <Card className="text-center">
+      <h2 className="text-center mb-4">Keunggulan Rumah Kusen Aluminium</h2>
+      <Row className='g-1'>
+        <Col lg={3} md={6} sm={6} xs={6} className="mb-4">
+          <Card className="text-center card-custom">
             <Card.Body>
-              <FaUsers size={50} className="mb-3 text-primary" />
-              <Card.Title>Tim Profesional</Card.Title>
+              <FaUsers className="mb-3 text-primary icon-custom" />
+              <Card.Title className="card-title-custom">Tim Profesional</Card.Title>
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={3} md={6} sm={6} xs={12} className="mb-4">
-          <Card className="text-center">
+        <Col lg={3} md={6} sm={6} xs={6} className="mb-4">
+          <Card className="text-center card-custom">
             <Card.Body>
-              <FaDollarSign size={50} className="mb-3 text-primary" />
-              <Card.Title>Harga Terjangkau</Card.Title>
+              <FaDollarSign className="mb-3 text-primary icon-custom" />
+              <Card.Title className="card-title-custom">Harga Terjangkau</Card.Title>
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={3} md={6} sm={6} xs={12} className="mb-4">
-          <Card className="text-center">
+        <Col lg={3} md={6} sm={6} xs={6} className="mb-4">
+          <Card className="text-center card-custom">
             <Card.Body>
-              <FaRocket size={50} className="mb-3 text-primary" />
-              <Card.Title>Proses Cepat</Card.Title>
+              <FaRocket className="mb-3 text-primary icon-custom" />
+              <Card.Title className="card-title-custom">Proses Cepat</Card.Title>
             </Card.Body>
           </Card>
         </Col>
-        <Col lg={3} md={6} sm={6} xs={12} className="mb-4">
-          <Card className="text-center">
+        <Col lg={3} md={6} sm={6} xs={6} className="mb-4">
+          <Card className="text-center card-custom">
             <Card.Body>
-              <FaShieldAlt size={50} className="mb-3 text-primary" />
-              <Card.Title>Bergaransi</Card.Title>
+              <FaShieldAlt className="mb-3 text-primary icon-custom" />
+              <Card.Title className="card-title-custom">Bergaransi</Card.Title>
             </Card.Body>
           </Card>
         </Col>
