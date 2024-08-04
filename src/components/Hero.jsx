f662b899dbc5800621e699e2611a5d557275340e
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-04 10:21:56 +0700

    new hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import '../assets/css/hero.css';
 
 function Hero() {
@@ -7,7 +7,7 @@ function Hero() {
     <div className="bg-dark text-white text-center py-5 hero-section">
       <Container>
         <h1>Selamat Datang di Website</h1>
-        <h2>RKA | Rumah Kusen Aluminium</h2>
+        <h2>Kusen Aluminium Window Home</h2>
         <div className="estetik-line"></div>
         <p>
           Kami merupakan spesialis jasa pembuatan dan pemasangan Kusen Pintu Aluminium, dan Jendela Aluminium. Segera Hubungi Kami untuk informasi selengkapnya.
