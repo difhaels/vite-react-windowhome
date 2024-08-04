0e1268211e31fa469955e0068ae3dcfd858d6cf1
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-04 21:10:28 +0700

    added mini service

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,16 @@ import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Superiority from '../components/Superiority';
 import Banner from '../components/Banner';
+import MiniServices from '../components/MiniServices';
 
 function Home() {
   return (
     <>
       <Header />
-      <Banner/>
+      <Banner />
       <Hero />
       <Superiority />
+      <MiniServices />
       <Footer />
     </>
   );
