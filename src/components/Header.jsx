e86e6229d1aafe7e7f6b984621f0845a368251c6
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-04 10:18:23 +0700

    features -> superiority

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../assets/css/header.css';
-import logo from '../assets/logo/logo.svg';
+import logo from '../assets/logo/logo.png';
 
 function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="#" className="navbar-brand-custom">
           <img
             src={logo}
             alt="Logo"
             className="brand-logo"
           />WindowHome
-          </Navbar.Brand>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
