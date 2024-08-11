428e43b07b0b404b2eaf29a9c6139f227c98ae53
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-11 07:56:14 +0700

    new mini products

--- src/components/MiniProducts.jsx
+++ src/components/MiniProducts.jsx
@@ -4,34 +4,34 @@ import image from "../assets/products/pintuKaca1.png";
 import "../assets/css/miniProducts.css";
 
 export default function MiniProducts() {
-    return (<div className="container bg-info">
+    return (<div className="container-mini-products">
         <Container className="px-5 pt-4">
             <div className="text-center">
                 <h5 className="mb-4 text-center" style={{ backgroundColor: '#222F99', color: 'white', display: 'inline-block', padding: '10px 90px'}}>Produk Kami</h5>
             </div>
             <Row>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
-                <Col lg={2} md={6} sm={12} className="mb-4">
+                <Col lg={2} xs={4} className="mb-4">
                     <img src={image} alt="" className="img-fluid" />
                 </Col>
             </Row>
