3459274ba25769f0f79635a48a658b7a599aaa92
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-12 06:00:07 +0700

    ubah header ke useNavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import '../assets/css/header.css';
 import logo from '../assets/logo/logo.png';
 
 function Header() {
+  const navigate = useNavigate();
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="/" className="navbar-brand-custom">
+        <Navbar.Brand onClick={()=>{navigate('/')}} className="navbar-brand-custom">
           <img
             src={logo}
             alt="Logo"
@@ -17,11 +19,11 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">Tentang Kami</Nav.Link>
-            <Nav.Link href="/services">Layanan</Nav.Link>
-            <Nav.Link href="/products">Produk</Nav.Link>
-            <Nav.Link href="/contact">Kontak</Nav.Link>
+            <Nav.Link  onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link  onClick={()=>{navigate('/about')}}>Tentang Kami</Nav.Link>
+            <Nav.Link  onClick={()=>{navigate('/services')}}>Layanan</Nav.Link>
+            <Nav.Link  onClick={()=>{navigate('/products')}}>Produk</Nav.Link>
+            <Nav.Link  onClick={()=>{navigate('/contact')}}>Kontak</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
