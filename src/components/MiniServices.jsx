577b9a384de85ade667ac040a3d98c6b66be8318
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-11 21:16:40 +0700

    layanan v1

--- src/components/MiniServices.jsx
+++ src/components/MiniServices.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import card1 from "../assets/products/pintuKaca1.png"
 
 export default function MiniServices() {
     return (<>
@@ -8,44 +9,36 @@ export default function MiniServices() {
                 <h5 className="mb-4 text-center" style={{ backgroundColor: '#222F99', color: 'white', display: 'inline-block', padding: '10px 90px'}}>Layanan Kami</h5>
             </div>
             <Row>
-                <Col lg={3} md={6} sm={12} className="mb-4">
+                <Col lg={3} xs={6} className="mb-4">
                     <Card className="text-center">
-                    <Card.Body>
-                        <Card.Title>Card 1</Card.Title>
-                        <Card.Text>
-                        This is card 1.
-                        </Card.Text>
-                    </Card.Body>
+                        <Card.Body>
+                            <Card.Img variant="top" src={card1} />
+                            <Button variant="success" className="mt-3">Pemasangan</Button>
+                        </Card.Body>
                     </Card>
                 </Col>
-                <Col lg={3} md={6} sm={12} className="mb-4">
+                <Col lg={3} xs={6} className="mb-4">
                     <Card className="text-center">
-                    <Card.Body>
-                        <Card.Title>Card 2</Card.Title>
-                        <Card.Text>
-                        This is card 2.
-                        </Card.Text>
-                    </Card.Body>
+                        <Card.Body>
+                            <Card.Img variant="top" src={card1} />
+                            <Button variant="success" className="mt-3">Pemasangan</Button>
+                        </Card.Body>
                     </Card>
                 </Col>
-                <Col lg={3} md={6} sm={12} className="mb-4">
+                <Col lg={3} xs={6} className="mb-4">
                     <Card className="text-center">
-                    <Card.Body>
-                        <Card.Title>Card 3</Card.Title>
-                        <Card.Text>
-                        This is card 3.
-                        </Card.Text>
-                    </Card.Body>
+                        <Card.Body>
+                            <Card.Img variant="top" src={card1} />
+                            <Button variant="success" className="mt-3">Pemasangan</Button>
+                        </Card.Body>
                     </Card>
                 </Col>
-                <Col lg={3} md={6} sm={12} className="mb-4">
+                <Col lg={3} xs={6} className="mb-4">
                     <Card className="text-center">
-                    <Card.Body>
-                        <Card.Title>Card 4</Card.Title>
-                        <Card.Text>
-                        This is card 4.
-                        </Card.Text>
-                    </Card.Body>
+                        <Card.Body>
+                            <Card.Img variant="top" src={card1} />
+                            <Button variant="success" className="mt-3">Pemasangan</Button>
+                        </Card.Body>
                     </Card>
                 </Col>
             </Row>
