de56df1d57af4fffc89ffd4bebb54a9ee235d253
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-12 06:10:22 +0700

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/app.css';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
       <Header />
+      <ScrollToTop/>
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
