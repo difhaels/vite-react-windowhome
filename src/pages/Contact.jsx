3ed301c5ec81082baacc554a2b9cc9916cc0927d
Agung Saputra <agungsaputradifh@gmail.com>
2024-08-19 08:49:28 +0700

    fix contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,22 @@
 import '../assets/css/contact.css';
 import ContactLeft from "../components/ContactLeft";
 import ContactRight from "../components/ContactRight";
-import { Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Contact() {
   return (
-    <>
-      <Row className="contact-container">
-        <h2 className="text-center mb-3">KONTAK</h2>
-        <Col xs={12} md={6} className="mb-4">
-          <ContactLeft />
-        </Col>
-        <Col xs={12} md={6} className="mb-4">
-          <ContactRight />
-        </Col>
-      </Row>
-    </>
+    <div className='contact-bg'>
+      <Container>
+        <Row className="contact-container">
+          <h2 className="text-center mb-3">KONTAK</h2>
+          <Col xs={12} md={6} className="mb-4">
+            <ContactLeft />
+          </Col>
+          <Col xs={12} md={6} className="mb-4">
+            <ContactRight />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 }
